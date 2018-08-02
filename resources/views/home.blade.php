b03db4d5d463985b7b3d255de74bc7298219d7e5
FelipeJB <felipejbq@hotmail.com>
2018-08-02 12:57:33 -0500

    Create home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,33 @@
           <ol class="breadcrumb">
             <li class="breadcrumb-item active">Inicio</li>
           </ol>
+          <div class="jumbotron">
+            <h2>Bienvenido/a {{ Auth::user()->name }}</h2>
+            <p>A continuación se listan las posibles acciones que puede realizar.</p><br>
 
+            @if(Auth::user()->role == 'Administrador')
+              <h3>Usuarios</h3>
+              <ul>
+                <li><a href="/AdministrarUsuarios">Administrar Usuarios</a></li>
+                <li><a href="/RegistrarUsuario">Registrar Usuario</a></li>
+              </ul>
+              <h3>Productos</h3>
+              <ul>
+                <li><a href="/AdministrarProductos">Administrar Productos</a></li>
+                <li><a href="/CrearProducto">Crear Producto</a></li>
+              </ul>
+              <h3>Puntos de Venta</h3>
+              <ul>
+                <li><a href="/AdministrarPuntos">Administrar Puntos de Venta</a></li>
+                <li><a href="/CrearPunto">Crear Punto de Venta</a></li>
+              </ul>
+              <h3>Datos</h3>
+              <ul>
+                <li><a href="/MigrarDatos">Migrar Datos</a></li>
+              </ul>
+            @endif
 
+          </div>
         </div>
     </div>
 </div>
