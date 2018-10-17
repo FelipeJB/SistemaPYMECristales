4cf6ef6fa9c32a7bcbc25a18515d4f39a233d48c
FelipeJB <felipejbq@hotmail.com>
2018-10-16 21:12:05 -0500

    Add Errors Handling

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }} - 404</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/silatorre.js') }}"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container">
+                <img src="{{ URL::asset('img/Logo-mid.png') }}" width="40" style="margin-right:10px">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                      <li class="nav-item">
+                          <a class="nav-link" href="/">Inicio</a>
+                      </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <main class="py-4">
+          <div class="container jumbotron">
+            <div class="row justify-content-center">
+              <div class="col-md-12">
+                <center>
+                  <h2 class="section-title">Página no encontrada</h2>
+                  <a class="btn btn-info btn-lg js-scroll-trigger" href="/">Ir al Inicio</a>
+                </center>
+                <hr>
+              </div>
+            </div>
+          </div>
+        </main>
+    </div>
+</body>
+</html>
